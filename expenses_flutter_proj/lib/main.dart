e091bff439659015831e7702ccf2bf210659a2bb
yousefh409 <57015880+yousefh409@users.noreply.github.com>
2020-10-29 12:55:20 -0700

    Update main.dart

--- expenses_flutter_proj/lib/main.dart
+++ expenses_flutter_proj/lib/main.dart
@@ -1,15 +1,17 @@
-import 'package:expenses_flutter_proj/Widgets/TransactionList.dart';
-import 'package:expenses_flutter_proj/transaction_data.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_expenses_app/Widgets/NewtTransaction.dart';
+import 'Widgets/TransactionList.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Expenses App',
       theme: ThemeData(
         errorColor: Colors.red,
         primarySwatch: Colors.purple,
@@ -23,11 +25,34 @@ class MyApp extends StatelessWidget {
               color: Colors.white,
             )),
       ),
-      title: 'Flutter Expenses App',
-      home: Scaffold(
-        body: TransactionList(
-          transactions: TransactionData.transactionData,
-        ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  void startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            onTap: () {},
+            child: NewTransaction(),
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: TransactionList(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => startAddNewTransaction(context),
       ),
     );
   }
