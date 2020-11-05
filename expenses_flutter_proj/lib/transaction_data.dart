befc2c1b6f9ab11c52bcf89c8ac017df3de1f302
yousefh409 <57015880+yousefh409@users.noreply.github.com>
2020-11-05 09:47:20 -0800

    Update transaction_data.dart

--- expenses_flutter_proj/lib/transaction_data.dart
+++ expenses_flutter_proj/lib/transaction_data.dart
@@ -1,19 +1,7 @@
-import 'package:expenses_flutter_proj/Models/transaction.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_expenses_app/Models/Transaction.dart';
+import 'package:provider/provider.dart';
 
-class TransactionData {
-  static final transactionData = [
-    Transaction(title: "Transaction 1", amount: 25.99, date: DateTime(2019, 1), id: "1"),
-    Transaction(title: "Transaction 2", amount: 1.99, date: DateTime(2019, 2), id: "2"),
-    Transaction(title: "Transaction 3", amount: 2645.99, date: DateTime(2019, 3), id: "3"),
-    Transaction(title: "Transaction 4", amount: 420.69, date: DateTime(2019, 4), id: "4"),
-    Transaction(title: "Transaction 5", amount: 22.29, date: DateTime(2019, 1), id: "5"),
-    Transaction(title: "Transaction 6", amount: 215.10, date: DateTime(2019, 5), id: "6"),
-    Transaction(title: "Transaction 7", amount: 0.01, date: DateTime(2019, 3), id: "7"),
-    Transaction(title: "Transaction 8", amount: 0.01, date: DateTime(2019, 20), id: "8"),
-    Transaction(title: "Transaction 9", amount: 1.01, date: DateTime(2019, 12), id: "9"),
-    Transaction(title: "Transaction 10", amount: 313.01, date: DateTime(2019, 231), id: "10"),
-    Transaction(title: "Transaction 11", amount: 0.01, date: DateTime(2019, 99), id: "11"),
-    Transaction(title: "Transaction 12", amount: 2.02, date: DateTime(2019, 17), id: "12"),
-
-  ];
+class TransactionData with ChangeNotifier {
+  var transactionData = [];
 }
