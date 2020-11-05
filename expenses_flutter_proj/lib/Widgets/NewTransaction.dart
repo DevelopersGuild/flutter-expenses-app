8b6fd6642a54560d56cd795c1727fa360ea00cd1
yousefh409 <57015880+yousefh409@users.noreply.github.com>
2020-11-05 09:50:12 -0800

    Update NewTransaction.dart

--- expenses_flutter_proj/lib/Widgets/NewTransaction.dart
+++ expenses_flutter_proj/lib/Widgets/NewTransaction.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_expenses_app/Data/transaction_data.dart';
+import 'package:flutter_expenses_app/Models/Transaction.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 class NewTransaction extends StatefulWidget {
   @override
@@ -6,69 +10,89 @@ class NewTransaction extends StatefulWidget {
 }
 
 class _NewTransactionState extends State<NewTransaction> {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+  TextEditingController titleController = TextEditingController();
+  TextEditingController amountController = TextEditingController();
   DateTime selectedDate = DateTime.now();
-
-  void submitData() {
-    String title = titleController.text;
-    String amount = amountController.text;
-    print(title);
-    print(amount);
-  }
-
-  void presentDatePicker() {}
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
+    void submitData() {
+      Provider.of<TransactionData>(context, listen: false).transactionData.add(Transaction(
+          title: titleController.text,
+          date: selectedDate,
+          amount: double.parse(amountController.text), id: DateTime.now().toString()),);
+      Provider.of<TransactionData>(context, listen: false).notifyListeners();
+
+      Navigator.of(context).pop();
+    }
+
+    void presentDatePicker() async {
+      DateTime picked = await showDatePicker(
+          context: context,
+          initialDate: selectedDate,
+          firstDate: DateTime(2015, 8),
+          lastDate: DateTime.now());
+
+      if (picked != null)
+        setState(() {
+          selectedDate = picked;
+        });
+    }
+
+    return Container(
       child: Padding(
-        padding: EdgeInsets.only(
-            left: 8.0,
-            right: 8.0,
-            top: 8.0,
-            bottom: MediaQuery.of(context).viewInsets.bottom + 10),
-        child: Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              TextField(
-                decoration: InputDecoration(labelText: "Title"),
-                controller: titleController,
-              ),
-              TextField(
-                decoration: InputDecoration(labelText: "Amount"),
-                controller: amountController,
-                keyboardType: TextInputType.number,
-              ),
-              Container(
-                height: 70,
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Text(
-                        selectedDate == null
-                            ? "No Date Chosen"
-                            : "Picked Date ${selectedDate.toString()}",
-                      ),
+        padding: EdgeInsets.all(8),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: "Title"),
+              controller: titleController,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: "Amount"),
+              controller: amountController,
+              keyboardType: TextInputType.number,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      selectedDate == null
+                          ? "No Date Chosen"
+                          : "Picked Date: ${DateFormat.yMMMd().format(selectedDate)}",
                     ),
-                    FlatButton(
-                      child: Text("Choose Date"),
-                      onPressed: () {
-                        presentDatePicker();
-                      },
-                    )
-                  ],
-                ),
+                  ),
+                  FlatButton(
+                    color: Theme.of(context).accentColor,
+                    child: Text("Choose Date"),
+                    onPressed: () {
+                      presentDatePicker();
+                    },
+                  )
+                ],
               ),
-              RaisedButton(
-                child: Text("Add Transactiomion"),
-                onPressed: () {
-                  submitData();
-                },
-              )
-            ],
-          ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: RaisedButton(
+                    color: Theme.of(context).primaryColor,
+                    child: Text(
+                      "Add Transactiomion",
+                      style: Theme.of(context).textTheme.button,
+                    ),
+                    onPressed: () {
+                      submitData();
+                    },
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
