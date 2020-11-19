b27d834cce4e99a64c95e6bd5a9dea986e1bc808
yousefh409 <57015880+yousefh409@users.noreply.github.com>
2020-11-19 12:51:59 -0800

    Update main.dart

--- expenses_flutter_proj/lib/main.dart
+++ expenses_flutter_proj/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_expenses_app/Data/transaction_data.dart';
-import 'package:flutter_expenses_app/Widgets/Chart.dart';
+import 'package:flutter_expenses_app/Widgets/ChartGraph.dart';
 import 'package:flutter_expenses_app/Widgets/NewTransaction.dart';
 import 'package:provider/provider.dart';
 import 'Widgets/TransactionList.dart';
@@ -25,7 +25,10 @@ class MyApp extends StatelessWidget {
             errorColor: Colors.red,
             primarySwatch: Colors.purple,
             accentColor: Colors.yellow,
-            textTheme: TextTheme(button: TextStyle(color: Colors.white))),
+            textTheme: TextTheme(
+              button: TextStyle(color: Colors.white),
+              headline1: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            )),
         home: MyHomePage(),
       ),
     );
@@ -35,6 +38,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   void startNewTransaction(BuildContext context) {
     showModalBottomSheet(
+        isScrollControlled: true,
         context: context,
         builder: (_) {
           return NewTransaction();
@@ -43,19 +47,17 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      resizeToAvoidBottomInset: false,
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
-        title: Text("Personal Expenses"),
-        actions: <Widget>[
+        title: Text("Expenses App"),
+        actions: [
           IconButton(
-            icon: Icon(
-              Icons.add,
-            ),
-            onPressed: () {
-              startNewTransaction(context);
-            },
-          )
+              icon: Icon(Icons.add),
+              onPressed: () {
+                startNewTransaction(context);
+              })
         ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
@@ -66,17 +68,19 @@ class MyHomePage extends StatelessWidget {
         },
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Container(
-            child: Chart(),
-            height: MediaQuery.of(context).size.height * 0.25,
-          ),
-          Container(
-            child: TransactionList(),
-            height: MediaQuery.of(context).size.height * 0.60,
-          ),
-        ],
+      body: Builder(
+        builder: (context) => Column(
+          children: [
+            Container(
+              child: ChartGraph(),
+              height: (height - Scaffold.of(context).appBarMaxHeight) * 0.25,
+            ),
+            Container(
+              child: TransactionList(),
+              height: (height - Scaffold.of(context).appBarMaxHeight) * 0.75,
+            ),
+          ],
+        ),
       ),
     );
   }
