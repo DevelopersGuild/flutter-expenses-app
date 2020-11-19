8d766a3489e1a42f18c963b49f8dd3825f47c79a
yousefh409 <57015880+yousefh409@users.noreply.github.com>
2020-11-19 12:53:53 -0800

    Update TransactionList.dart

--- expenses_flutter_proj/lib/Widgets/TransactionList.dart
+++ expenses_flutter_proj/lib/Widgets/TransactionList.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_expenses_app/Models/Transaction.dart';
 import 'package:provider/provider.dart';
 import '../Widgets/TransactionItem.dart';
@@ -7,17 +8,36 @@ import '../Data/transaction_data.dart';
 class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var transactionData =
+        Provider.of<TransactionData>(context, listen: true).transactionData;
     return Container(
-      child: ListView.builder(
-        itemBuilder: (ctx, index) {
-          return TransactionItem(
-            transaction: Provider.of<TransactionData>(context, listen: true)
-                .transactionData[index],
-          );
-        },
-        itemCount: Provider.of<TransactionData>(context, listen: true)
-            .transactionData.length,
-      ),
+      child: transactionData.isEmpty
+          ? Column(
+              // Column()
+              children: <Widget>[
+                Text(
+                  "No Transactions added yet!",
+                  style: Theme.of(context).textTheme.headline1,
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    "assets/image/waiting.png",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return TransactionItem(
+                  transaction: transactionData[index],
+                );
+              },
+              itemCount: transactionData.length),
     );
   }
 }
